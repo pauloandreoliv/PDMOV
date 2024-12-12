d012571e73fe518e635c3b413b0a4835558ddf42
Paulo André <pauloandrejanga@gmail.com>
2024-12-12 08:39:21 -0300

    CadastroActivity

--- MaisPaulista/app/src/main/java/com/projeto/maispaulista/MainActivity.kt
+++ MaisPaulista/app/src/main/java/com/projeto/maispaulista/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.projeto.maispaulista
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Configurar insets para o layout raiz
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Configurar clique no TextView para redirecionar à CadastroActivity
+        val registerLink = findViewById<TextView>(R.id.registerLink)
+        registerLink.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
